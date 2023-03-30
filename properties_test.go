35cd405584b28eb081bbfc7125167aa21017f62b
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-03-30 23:07:17 +0100

    test: use testify

--- properties_test.go
+++ properties_test.go
@@ -1,7 +1,7 @@
 package dalgo2gaedatastore
 
 import (
-	"github.com/matryer/is"
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/appengine/datastore"
 	"testing"
 	"time"
@@ -9,42 +9,41 @@ import (
 
 func TestIsEmptyJson(t *testing.T) {
 	t.Parallel()
-	is := is.New(t)
+	//is := is.New(t)
 
 	p := datastore.Property{
 		Name:    "TestProp1",
 		Value:   nil,
 		NoIndex: true,
 	}
-	is.True(IsEmptyJSON(p)) // nil should be treated as empty string
+	assert.True(t, IsEmptyJSON(p)) // nil should be treated as empty string
 
 	p.Value = ""
-	is.True(IsEmptyJSON(p)) // Empty string should return true"
+	assert.True(t, IsEmptyJSON(p)) // Empty string should return true"
 
 	p.Value = "[]"
-	is.True(IsEmptyJSON(p)) // Empty string should return true"
+	assert.True(t, IsEmptyJSON(p)) // Empty string should return true"
 
 	p.Value = "{}"
-	is.True(IsEmptyJSON(p)) // Empty string should return true"
+	assert.True(t, IsEmptyJSON(p)) // Empty string should return true"
 
 	p.Value = "0"
-	is.True(!IsEmptyJSON(p)) // '0' string should return false"
+	assert.False(t, IsEmptyJSON(p)) // '0' string should return false"
 }
 
 func TestIsZeroTime(t *testing.T) {
 	t.Parallel()
-	is := is.New(t)
 
 	p := datastore.Property{
 		Name:    "TestProp1",
 		Value:   nil,
 		NoIndex: true,
 	}
-	is.True(IsZeroTime(p)) // nil should be treated as zero value
+	assert.True(t, IsZeroTime(p)) // nil should be treated as zero value
 
 	p.Value = time.Time{}
-	is.True(IsZeroTime(p)) // should return true for zero value
+	assert.True(t, IsZeroTime(p)) // should return true for zero value
 
 	p.Value = time.Now()
-	is.True(!IsZeroTime(p)) //  string should return false for time.Now()
+	assert.False(t, IsZeroTime(p)) //  string should return false for time.Now()
 }
